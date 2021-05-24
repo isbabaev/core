90c43e4be9a5592b35c9cc5952bfdef892fbac76
Islam Babaev <48277358+isbabaev@users.noreply.github.com>
2021-05-24 20:16:32 +0300

    Добавил в тесты проверку на то, что аккаунт может быть уже в бд

--- test/create-account.e2e-spec.ts
+++ test/create-account.e2e-spec.ts
@@ -14,26 +14,27 @@ import { CreateAccountDto } from '../src/modules/api/dto/create-account.dto';
 import * as request from 'supertest';
 import { ICreateAccountResult } from '../src/domains/ports/in/create-account/create-account.result';
 import * as Joi from 'joi';
+import { Account } from '../src/domains/entities/account';
 
 describe('CreateAccountE2eTest', () => {
   let app: INestApplication;
   let mockedAddAccountToPersistencePort: IAddAccountToPersistencePort;
-  let mockedLoadAccountToEmailPort: ILoadAccountByEmailPort;
+  let mockedLoadAccountByEmailPort: ILoadAccountByEmailPort;
   let createAccountData: CreateAccountDto;
 
   beforeAll(async () => {
     mockedAddAccountToPersistencePort = mock<IAddAccountToPersistencePort>();
-    mockedLoadAccountToEmailPort = mock<ILoadAccountByEmailPort>();
+    mockedLoadAccountByEmailPort = mock<ILoadAccountByEmailPort>();
 
     when(mockedAddAccountToPersistencePort.addAccountToPersistence(anything())).thenResolve();
-    when(mockedLoadAccountToEmailPort.loadAccountByEmail(anything())).thenResolve(null);
+    when(mockedLoadAccountByEmailPort.loadAccountByEmail(anything())).thenResolve(null);
 
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).overrideProvider(AddAccountToPersistencePortSymbol)
       .useValue(Object.assign({}, instance(mockedAddAccountToPersistencePort)))
       .overrideProvider(LoadAccountByEmailPortSymbol)
-      .useValue(Object.assign({}, instance(mockedLoadAccountToEmailPort)))
+      .useValue(Object.assign({}, instance(mockedLoadAccountByEmailPort)))
       .compile();
 
     app = moduleRef.createNestApplication();
@@ -67,6 +68,15 @@ describe('CreateAccountE2eTest', () => {
 
     const responseBody = response.body as ICreateAccountResult;
     Joi.assert(responseBody.id, Joi.string().uuid().required());
+  });
+
+  test('should return error when account is exists', async () => {
+    when(mockedLoadAccountByEmailPort.loadAccountByEmail(anything())).thenResolve(mock(Account));
+
+    await request(app.getHttpServer())
+      .post('/create-account')
+      .send(createAccountData)
+      .expect(HttpStatus.INTERNAL_SERVER_ERROR);
   });
 
 });
