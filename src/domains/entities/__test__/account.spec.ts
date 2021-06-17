27ca71f4b8575b4b83338665465ae8cfad168c9d
Islam Babaev <48277358+isbabaev@users.noreply.github.com>
2021-06-17 16:18:58 +0300

    Рефакторинг Account. Вынес логику установки значений в сеттеры

--- src/domains/entities/__test__/account.spec.ts
+++ src/domains/entities/__test__/account.spec.ts
@@ -5,18 +5,294 @@ import { Id } from '../../value-objects/id';
 import { AccountEmail } from '../../value-objects/account/account-email';
 import { AccountPassword } from '../../value-objects/account/account-password';
 import { v4 as uuidv4 } from 'uuid';
+import { AccountRole } from '../../value-objects/account/account-role';
 
 describe('AccountTest', () => {
-  test('should create Account instance', () => {
-    const id = uuidv4();
-    const account = new Account(
-      new Id(id),
+  let account: Account;
+  let requestAccount: Account;
+
+  beforeAll(() => {
+    const uuid = uuidv4();
+    account = new Account(
+      new Id(uuid),
       new AccountFirstName('firstName'),
       new AccountLastName('lastName'),
       new AccountEmail('mail@mail.com'),
       new AccountPassword('password'),
+      new AccountRole('user'),
     );
 
+    requestAccount = new Account(
+      new Id(uuid),
+      new AccountFirstName('firstName'),
+      new AccountLastName('lastName'),
+      new AccountEmail('mail@mail.com'),
+      new AccountPassword('password'),
+      new AccountRole('user'),
+    );
+  });
+
+  test('should create Account instance', () => {
     expect(account).toBeInstanceOf(Account);
+  });
+
+  test('should throw error "firstName is null or undefined" when firstName is null', () => {
+    // account.firstName =
+    expect(account).toBeInstanceOf(Account);
+  });
+
+  describe('setFirstName', () => {
+    let newFirstName: AccountFirstName;
+
+    beforeAll(() => {
+      newFirstName = new AccountFirstName('NewFirstName');
+    });
+
+    test('should change account firstName', () => {
+      account.setFirstName(newFirstName, requestAccount);
+
+      expect(account.getFirstName()).toEqual(newFirstName);
+    });
+
+    test(`should throw error "the user does not have access to edit the firstName" 
+                when requestAccount id is not equal to account id`, () => {
+      const _requestAccount = new Account(
+        new Id(uuidv4()),
+        new AccountFirstName('firstName'),
+        new AccountLastName('lastName'),
+        new AccountEmail('mail@mail.com'),
+        new AccountPassword('password'),
+        new AccountRole('user'),
+      );
+
+      expect(() => account.setFirstName(newFirstName, _requestAccount))
+        .toThrowError('the user does not have access to edit the firstName');
+    });
+
+    test(`should throw error "requestAccount is null or undefined" when requestAccount is null`, () => {
+      const requestAccount = null;
+
+      expect(() => account.setFirstName(newFirstName, requestAccount))
+        .toThrowError('requestAccount is null or undefined');
+    });
+
+    test(`should throw error "firstName is null or undefined" when firstName is null`, () => {
+      const _newFirstName = null;
+
+      expect(() => account.setFirstName(_newFirstName, requestAccount))
+        .toThrowError('firstName is null or undefined');
+    });
+
+    test(`should update updateAt`, () => {
+      const oldUpdateAt = account.updatedAt;
+
+      account.setFirstName(newFirstName, requestAccount);
+
+      expect(account.updatedAt.value.getTime()).toBeGreaterThan(oldUpdateAt.value.getTime());
+    });
+  });
+
+  describe('setLastName', () => {
+    let newLastName: AccountLastName;
+
+    beforeAll(() => {
+      newLastName = new AccountLastName('NewLastName');
+    });
+
+    test('should change account lastName', () => {
+      account.setLastName(newLastName, requestAccount);
+
+      expect(account.getLastName()).toEqual(newLastName);
+    });
+
+    test(`should throw error "the user does not have access to edit the lastName" 
+                when requestAccount id is not equal to account id`, () => {
+      const _requestAccount = new Account(
+        new Id(uuidv4()),
+        new AccountFirstName('firstName'),
+        new AccountLastName('lastName'),
+        new AccountEmail('mail@mail.com'),
+        new AccountPassword('password'),
+        new AccountRole('user'),
+      );
+
+      expect(() => account.setLastName(newLastName, _requestAccount))
+        .toThrowError('the user does not have access to edit the lastName');
+    });
+
+    test(`should throw error "requestAccount is null or undefined" when requestAccount is null`, () => {
+      const requestAccount = null;
+
+      expect(() => account.setLastName(newLastName, requestAccount))
+        .toThrowError('requestAccount is null or undefined');
+    });
+
+    test(`should throw error "lastName is null or undefined" when lastName is null`, () => {
+      const _newLastName = null;
+
+      expect(() => account.setLastName(_newLastName, requestAccount))
+        .toThrowError('lastName is null or undefined');
+    });
+
+    test(`should update updateAt`, () => {
+      const oldUpdateAt = account.updatedAt;
+
+      account.setLastName(newLastName, requestAccount);
+
+      expect(account.updatedAt.value.getTime()).toBeGreaterThan(oldUpdateAt.value.getTime());
+    });
+  });
+
+  describe('setEmail', () => {
+    let newEmail: AccountEmail;
+
+    beforeAll(() => {
+      newEmail = new AccountEmail('test@mail.com');
+    });
+
+    test('should change account email', () => {
+      account.setEmail(newEmail, requestAccount);
+
+      expect(account.getEmail()).toEqual(newEmail);
+    });
+
+    test(`should throw error "the user does not have access to edit the email" 
+                when requestAccount id is not equal to account id`, () => {
+      const _requestAccount = new Account(
+        new Id(uuidv4()),
+        new AccountFirstName('firstName'),
+        new AccountLastName('lastName'),
+        new AccountEmail('mail@mail.com'),
+        new AccountPassword('password'),
+        new AccountRole('user'),
+      );
+
+      expect(() => account.setEmail(newEmail, _requestAccount))
+        .toThrowError('the user does not have access to edit the email');
+    });
+
+    test(`should throw error "requestAccount is null or undefined" when requestAccount is null`, () => {
+      const requestAccount = null;
+
+      expect(() => account.setEmail(newEmail, requestAccount))
+        .toThrowError('requestAccount is null or undefined');
+    });
+
+    test(`should throw error "email is null or undefined" when email is null`, () => {
+      const _newEmail = null;
+
+      expect(() => account.setEmail(_newEmail, requestAccount))
+        .toThrowError('email is null or undefined');
+    });
+
+    test(`should update updateAt`, () => {
+      const oldUpdateAt = account.updatedAt;
+
+      account.setEmail(newEmail, requestAccount);
+
+      expect(account.updatedAt.value.getTime()).toBeGreaterThan(oldUpdateAt.value.getTime());
+    });
+  });
+
+  describe('setPassword', () => {
+    let newPassword: AccountPassword;
+
+    beforeAll(() => {
+      newPassword = new AccountPassword('password');
+    });
+
+    test('should change account password', () => {
+      account.setPassword(newPassword, requestAccount);
+
+      expect(account.getPassword()).toEqual(newPassword);
+    });
+
+    test(`should throw error "the user does not have access to edit the password" 
+                when requestAccount id is not equal to account id`, () => {
+      const _requestAccount = new Account(
+        new Id(uuidv4()),
+        new AccountFirstName('firstName'),
+        new AccountLastName('lastName'),
+        new AccountEmail('mail@mail.com'),
+        new AccountPassword('password'),
+        new AccountRole('user'),
+      );
+
+      expect(() => account.setPassword(newPassword, _requestAccount))
+        .toThrowError('the user does not have access to edit the password');
+    });
+
+    test(`should throw error "requestAccount is null or undefined" when requestAccount is null`, () => {
+      const requestAccount = null;
+
+      expect(() => account.setPassword(newPassword, requestAccount))
+        .toThrowError('requestAccount is null or undefined');
+    });
+
+    test(`should throw error "password is null or undefined" when password is null`, () => {
+      const _newPassword = null;
+
+      expect(() => account.setPassword(_newPassword, requestAccount))
+        .toThrowError('password is null or undefined');
+    });
+
+    test(`should update updateAt`, () => {
+      const oldUpdateAt = account.updatedAt;
+
+      account.setPassword(newPassword, requestAccount);
+
+      expect(account.updatedAt.value.getTime()).toBeGreaterThan(oldUpdateAt.value.getTime());
+    });
+  });
+
+  describe('setPassword', () => {
+    let newPassword: AccountPassword;
+
+    beforeAll(() => {
+      newPassword = new AccountPassword('password');
+    });
+
+    test('should change account password', () => {
+      account.setPassword(newPassword, requestAccount);
+
+      expect(account.getPassword()).toEqual(newPassword);
+    });
+
+    test(`should throw error "the user does not have access to edit the password" 
+                when requestAccount id is not equal to account id`, () => {
+      const _requestAccount = new Account(
+        new Id(uuidv4()),
+        new AccountFirstName('firstName'),
+        new AccountLastName('lastName'),
+        new AccountEmail('mail@mail.com'),
+        new AccountPassword('password'),
+        new AccountRole('user'),
+      );
+
+      expect(() => account.setPassword(newPassword, _requestAccount))
+        .toThrowError('the user does not have access to edit the password');
+    });
+
+    test(`should throw error "requestAccount is null or undefined" when requestAccount is null`, () => {
+      const requestAccount = null;
+
+      expect(() => account.setPassword(newPassword, requestAccount))
+        .toThrowError('requestAccount is null or undefined');
+    });
+
+    test(`should throw error "password is null or undefined" when password is null`, () => {
+      const _newPassword = null;
+
+      expect(() => account.setPassword(_newPassword, requestAccount))
+        .toThrowError('password is null or undefined');
+    });
+
+    test(`should update updateAt`, () => {
+      const oldUpdateAt = account.updatedAt;
+
+      account.setPassword(newPassword, requestAccount);
+
+      expect(account.updatedAt.value.getTime()).toBeGreaterThan(oldUpdateAt.value.getTime());
+    });
   });
 });
--- src/domains/entities/account.ts
+++ src/domains/entities/account.ts
@@ -5,6 +5,7 @@ import { AccountLastName } from '../value-objects/account/account-last-name';
 import { AccountPassword } from '../value-objects/account/account-password';
 import { CreatedAt } from '../value-objects/created-at';
 import { UpdatedAt } from '../value-objects/updated-at';
+import { AccountRole } from '../value-objects/account/account-role';
 
 export class Account {
   private readonly _id: Id;
@@ -14,49 +15,50 @@ export class Account {
   private _password: AccountPassword;
   private readonly _createdAt: CreatedAt;
   private _updatedAt: UpdatedAt;
+  private _role: AccountRole;
+
+  private set firstName(value: AccountFirstName) {
+    if (value == null) {
+      throw new Error('firstName is null or undefined');
+    }
+
+    this._firstName = value;
+  }
+
+  private set lastName(value: AccountLastName) {
+    if (value == null) {
+      throw new Error('lastName is null or undefined');
+    }
+
+    this._lastName = value;
+  }
+
+  private set email(value: AccountEmail) {
+    if (value == null) {
+      throw new Error('email is null or undefined');
+    }
+
+    this._email = value;
+  }
+
+  private set password(value: AccountPassword) {
+    if (value == null) {
+      throw new Error('password is null or undefined');
+    }
+
+    this._password = value;
+  }
+
+  private set role(value: AccountRole) {
+    if (value == null) {
+      throw new Error('role is null or undefined');
+    }
+
+    this._role = value;
+  }
 
   get id(): Id {
     return this._id;
-  }
-
-  get firstName(): AccountFirstName {
-    return this._firstName;
-  }
-
-  set firstName(value: AccountFirstName) {
-    this._firstName = value;
-
-    this.changeUpdatedAt();
-  }
-
-  get lastName(): AccountLastName {
-    return this._lastName;
-  }
-
-  set lastName(value: AccountLastName) {
-    this._lastName = value;
-
-    this.changeUpdatedAt();
-  }
-
-  get email(): AccountEmail {
-    return this._email;
-  }
-
-  set email(value: AccountEmail) {
-    this._email = value;
-
-    this.changeUpdatedAt();
-  }
-
-  get password(): AccountPassword {
-    return this._password;
-  }
-
-  set password(value: AccountPassword) {
-    this._password = value;
-
-    this.changeUpdatedAt();
   }
 
   get createdAt(): CreatedAt {
@@ -67,27 +69,113 @@ export class Account {
     return this._updatedAt;
   }
 
-  set updatedAt(value: UpdatedAt) {
-    this._updatedAt = value;
-
-    this.changeUpdatedAt();
-  }
-
   constructor(id: Id,
               firstName: AccountFirstName,
               lastName: AccountLastName,
               email: AccountEmail,
-              password: AccountPassword) {
+              password: AccountPassword,
+              role: AccountRole) {
+    if (id == null) {
+      throw new Error('id is null or undefined');
+    }
+
     this._id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.password = password;
+    this.role = role;
     this._createdAt = new CreatedAt(new Date());
-    this.changeUpdatedAt();
+    this.updateUpdatedAt();
   }
 
-  private changeUpdatedAt(): void {
+  private updateUpdatedAt(): void {
     this._updatedAt = new UpdatedAt(new Date());
+  }
+
+  setFirstName(firstName: AccountFirstName, requestAccount: Account): void {
+    if (requestAccount == null) {
+      throw new Error('requestAccount is null or undefined');
+    }
+
+    if (!requestAccount.id.equalsTo(this.id)) {
+      throw new Error('the user does not have access to edit the firstName');
+    }
+
+    this.firstName = firstName;
+
+    this.updateUpdatedAt();
+  }
+
+  setLastName(lastName: AccountLastName, requestAccount: Account): void {
+    if (requestAccount == null) {
+      throw new Error('requestAccount is null or undefined');
+    }
+
+    if (!requestAccount.id.equalsTo(this.id)) {
+      throw new Error('the user does not have access to edit the lastName');
+    }
+
+    this.lastName = lastName;
+
+    this.updateUpdatedAt();
+  }
+
+  setEmail(email: AccountEmail, requestAccount: Account): void {
+    if (requestAccount == null) {
+      throw new Error('requestAccount is null or undefined');
+    }
+
+    if (!requestAccount.id.equalsTo(this.id)) {
+      throw new Error('the user does not have access to edit the email');
+    }
+
+    this.email = email;
+
+    this.updateUpdatedAt();
+  }
+
+  setPassword(password: AccountPassword, requestAccount: Account): void {
+    if (requestAccount == null) {
+      throw new Error('requestAccount is null or undefined');
+    }
+
+    if (!requestAccount.id.equalsTo(this.id)) {
+      throw new Error('the user does not have access to edit the password');
+    }
+
+    this.password = password;
+
+    this.updateUpdatedAt();
+  }
+
+  setRole(role: AccountRole, requestAccount: Account): void {
+    /*if (requestAccount == null) {
+      throw new Error('requestAccount is null or undefined');
+    }
+
+    if (!requestAccount.id.equalsTo(this.id)) {
+      throw new Error('the user does not have access to edit the role');
+    }
+
+    this.role = role;
+
+    this.updateUpdatedAt();*/
+  }
+
+  getFirstName(): AccountFirstName {
+    return this._firstName;
+  }
+
+  getLastName(): AccountLastName {
+    return this._lastName;
+  }
+
+  getEmail(): AccountEmail {
+    return this._email;
+  }
+
+  getPassword(): AccountPassword {
+    return this._password;
   }
 }
